1361eaea007a972ad19bf59d9e43a80a0151f3d5
daniistiqomah <35377683+daniistiqomah@users.noreply.github.com>
2018-02-04 22:10:15 +0700

    Update called.js

--- called.js
+++ called.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/01/cloud-computing-is-changing-way.html"}
 if(x=="2"){xxx="/2018/02/the-world-wide-forex-market.html"}
 if(x=="3"){xxx="/2018/01/go-link.html"}
 if(x=="4"){xxx="/2018/01/go.html"}
-if(x=="5"){xxx="/2018/01/go-to.html"}a_to_ve[i].href="https://drive-googlle-com.blogspot.co.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/01/go-to.html"}a_to_ve[i].href="http://drive.carakuy.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
